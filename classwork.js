d3cc3b8badb963fcfea978178e673089ba9396f3
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-02-04 23:47:26 -0500

    major credit to Emily Bullard for this particle object

--- classwork.js
+++ classwork.js
@@ -7,3 +7,15 @@ var particles = []
 
 var time = 0;
 var gravity = 0.5;
+
+Particle.prototype = {
+  getVelocity: function(){
+    return time * gravity;
+  },
+  move: function(){
+    this.y += this.getVelocity();
+    if (this.y >= 500){
+      console.log("bottom");
+    }
+  }
+}
